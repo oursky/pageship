29ba1b437c77f81c10ab7e2ddda222c6e5f8b1f0
kiootic <kiootic@oursky.com>
2023-05-11 16:07:07 +0800

    Add test server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/oursky/pageship/internal/cmd"
+	"go.uber.org/zap"
+)
+
+func serve(ctx context.Context) error {
+	server := http.Server{
+		Addr: ":8000",
+	}
+
+	shutdown := make(chan struct{})
+	go func() {
+		<-ctx.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		server.Shutdown(ctx)
+		close(shutdown)
+	}()
+
+	err := server.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+	<-shutdown
+
+	return nil
+}
 
 func main() {
-	fmt.Println("hello world!")
+	cfg := zap.NewDevelopmentConfig()
+	logger, _ := cfg.Build()
+	defer logger.Sync()
+
+	cmd.Run(logger, []cmd.WorkFunc{serve})
 }
---
+++ internal/cmd/run.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+)
+
+type WorkFunc func(context.Context) error
+
+func Run(logger *zap.Logger, works []WorkFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+
+	logger.Debug("starting...")
+	for _, f := range works {
+		work := f
+		g.Go(func() error { return work(ctx) })
+	}
+
+	go func() {
+		<-sig
+		logger.Debug("stopping...")
+		cancel()
+	}()
+
+	_ = g.Wait()
+}
