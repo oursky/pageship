2e9a206240f1e96644c381beb6bd44c444926f5c
mark wu <saybyebyet@gmail.com>
2023-12-06 15:47:59 +0800

    Accept hostname with port as domain

--- internal/config/app_domain.go
+++ internal/config/app_domain.go
@@ -1,6 +1,6 @@
 package config
 
 type AppDomainConfig struct {
-	Domain string `json:"domain" pageship:"required,max=200,hostname_rfc1123,lowercase"`
+	Domain string `json:"domain" pageship:"required,max=200,hostname_port|hostname_rfc1123,lowercase"`
 	Site   string `json:"site" pageship:"required,dnsLabel"`
 }
