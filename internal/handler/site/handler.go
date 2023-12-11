3341744fc119395eb345f1d9880e81cb4f306476
mark wu <saybyebyet@gmail.com>
2023-12-11 15:52:48 +0800

    Skip api log during testing

--- internal/handler/site/handler.go
+++ internal/handler/site/handler.go
@@ -136,8 +136,11 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.logger.Debug("resolved site", zap.String("site", handler.ID()))
-	entry := middleware.GetLogEntry(r).(*httputil.LogEntry)
-	entry.Logger = entry.Logger.With(zap.String("site", handler.ID()))
+	entry := middleware.GetLogEntry(r)
+	if entry != nil {
+		e := entry.(*httputil.LogEntry)
+		e.Logger = e.Logger.With(zap.String("site", handler.ID()))
+	}
 
 	if err := h.checkAuthz(r, handler); err != nil {
 		http.NotFound(w, r)
