3c263ea83598bb39d2ff9d760e6464e839664946
mark wu <saybyebyet@gmail.com>
2023-12-15 17:08:21 +0800

    No need to check http log instance

--- internal/handler/site/handler.go
+++ internal/handler/site/handler.go
@@ -137,10 +137,8 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	h.logger.Debug("resolved site", zap.String("site", handler.ID()))
 	entry := middleware.GetLogEntry(r)
-	if entry != nil {
-		e := entry.(*httputil.LogEntry)
-		e.Logger = e.Logger.With(zap.String("site", handler.ID()))
-	}
+	e := entry.(*httputil.LogEntry)
+	e.Logger = e.Logger.With(zap.String("site", handler.ID()))
 
 	if err := h.checkAuthz(r, handler); err != nil {
 		http.NotFound(w, r)
