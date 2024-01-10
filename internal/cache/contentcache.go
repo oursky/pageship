c2adf488eb9c40a8edbd5344770e87d61d3ecf88
toshinari123 <toshinaritong@gmail.com>
2024-01-10 16:39:24 +0800

    model type ContentCache (real)

---
+++ internal/cache/contentcache.go
@@ -0,0 +1,18 @@
+package cache
+
+import (
+	"io"
+	"sync"
+
+	"github.com/dgraph-io/ristretto"
+)
+
+const (
+	contentCacheSize uint64    		= 1000000000
+)
+
+type ContentCache[T io.WriteCloser] struct {
+	m     sync.Mutex
+	cache *ristretto.Cache
+	load  func(id string) (T, error)
+}
