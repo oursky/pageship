28f1f8600252e01a8c7d50bc0d2703ece538966c
toshinari123 <toshinaritong@gmail.com>
2024-01-10 16:50:39 +0800

    write func NewContentCache

--- internal/cache/contentcache.go
+++ internal/cache/contentcache.go
@@ -8,11 +8,24 @@ import (
 )
 
 const (
-	contentCacheSize uint64 = 1000000000
+	contentCacheSize int64 = 1000000000
 )
 
 type ContentCache[T io.WriteCloser] struct {
 	m     map[string]sync.Mutex
 	cache *ristretto.Cache
 	load  func(id string) (T, error)
+}
+
+func NewContentCache[T io.WriteCloser](load func(id string) (T, error)) (*ContentCache[T], error) {
+	cache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 1e7,
+		MaxCost:     contentCacheSize,
+		BufferItems: 64,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &ContentCache[T]{cache: cache, load: load}, nil
 }
