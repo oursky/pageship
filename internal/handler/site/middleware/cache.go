d3272ba8b529486865482496e25ec6d8bdfcbb6f
toshinari123 <toshinaritong@gmail.com>
2024-02-05 15:52:54 +0800

    Include encoding in contentcache key

--- internal/handler/site/middleware/cache.go
+++ internal/handler/site/middleware/cache.go
@@ -36,7 +36,14 @@ func (ctx *CacheContext) Cache(site *site.Descriptor, next http.Handler) http.Ha
 			return
 		}
 
-		value, found := ctx.cc.GetContent(ContentCacheKey{hash: info.Hash, compression: "none"}) //TODO: find compression type
+		compression := "no"
+		if contains(r.Header["Accept-Encoding"], "*") || contains(r.Header["Accept-Encoding"], "br") {
+			compression := "br"
+		} else if contains(r.Header["Accept-Encoding"], "gzip") {
+			compression := "gz"
+		}
+
+		value, found := ctx.cc.GetContent(ContentCacheKey{hash: info.Hash, compression: compression})
 		if found {
 			reader := bytes.NewReader(value.Bytes())
 			writer := httputil.NewTimeoutResponseWriter(w, 10*time.Second)
@@ -46,11 +53,27 @@ func (ctx *CacheContext) Cache(site *site.Descriptor, next http.Handler) http.Ha
 
 		rec := httptest.NewRecorder()
 		next.ServeHTTP(rec, r)
-
 		reader := bytes.NewReader(rec.Body.Bytes())
-		ctx.cc.SetContent(ContentCacheKey{hash: info.Hash, compression: "none"}, reader)
+
+		compression = "no"
+		if contains(rec.Header["Content-Encoding"], "*") || contains(rec.Header["Content-Encoding"], "br") {
+			compression = "br"
+		} else if contains(rec.Header["Content-Encoding"], "gzip") {
+			compression = "gz"
+		}
+
+		ctx.cc.SetContent(ContentCacheKey{hash: info.Hash, compression: compression}, reader)
 		writer := httputil.NewTimeoutResponseWriter(w, 10*time.Second)
 		http.ServeContent(writer, r, path.Base(r.URL.Path), info.ModTime, reader)
 	})
 }
 
+//https://stackoverflow.com/questions/10485743/contains-method-for-a-slice
+func contains(s []string, e string) bool {
+    for _, a := range s {
+        if a == e {
+            return true
+        }
+    }
+    return false
+}
